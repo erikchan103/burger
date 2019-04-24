fc739a57e8a82bee05c5984bdf62b33b149adb09
erikchan103 <erik.chan@sugarmade.com>
2019-04-23 22:59:49 -0700

    server and site up. no burger name

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,13 @@
-CREATE DATABASE `burgers_db`;
-USE `burgers_db`;
+CREATE DATABASE burgers_db;
+USE burgers_db;
 
-CREATE TABLE `burgers` (
-    `id` Int( 11 ) AUTO_INCREMENT NOT NULL,
-    `burger_name` VARCHAR( 255) NOT NULL,
-    `devoured` BOOLEAN NOT NULL
-)
+CREATE TABLE burgers
+(
+    id int
+    AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR
+    (255) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY
+    (id)
+);
